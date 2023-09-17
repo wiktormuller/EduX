5ad3514278734ca675e9564a6abcf14a0a84031d
Wiktor Muller <biznes.wiktor@gmail.com>
2023-09-17 19:06:11 +0200

    feat(tests): Add first performance test with NBomber

---
+++ tests/Modules/Users/Edux.Modules.Users.Tests.Performance/EduxApiPerformanceTests.cs
@@ -0,0 +1,55 @@
+﻿using NBomber.CSharp;
+using NBomber.Http.CSharp;
+using Shouldly;
+using Xunit.Abstractions;
+
+namespace Edux.Modules.Users.Tests.Performance
+{
+    public class EduxApiPerformanceTests
+    {
+        private readonly ITestOutputHelper _outputHelper;
+
+        public EduxApiPerformanceTests(ITestOutputHelper outputHelper)
+        {
+            _outputHelper = outputHelper;
+        }
+
+        [Fact]
+        public void root_endpoint_from_bootstrapper_handle_at_least_100_requests_per_seconds()
+        {
+            // Arrange
+            const string url = "http://localhost:5000/";
+
+            using var httpClient = new HttpClient();
+
+            const int expectedRequestsPerSeconds = 100;
+            const int durationSeconds = 5;
+
+            var getRootScenario = Scenario.Create("get root scenario", async context =>
+            {
+                try
+                {
+                    var request = Http.CreateRequest("GET", url);
+
+                    var response = await Http.Send(httpClient, request);
+                    return response;
+                }
+                catch
+                {
+                    return Response.Fail();
+                }
+            })
+            .WithWarmUpDuration(TimeSpan.FromSeconds(5))
+            .WithLoadSimulations(Simulation.KeepConstant(copies: expectedRequestsPerSeconds, during: TimeSpan.FromSeconds(durationSeconds)));
+
+            // Act
+            var stats = NBomberRunner
+                .RegisterScenarios(getRootScenario)
+                .Run();
+
+            // Assert
+            _outputHelper.WriteLine($"OK: {stats.AllOkCount}, FAILED: {stats.AllFailCount}.");
+            stats.AllOkCount.ShouldBeGreaterThanOrEqualTo(durationSeconds * expectedRequestsPerSeconds);
+        }
+    }
+}
