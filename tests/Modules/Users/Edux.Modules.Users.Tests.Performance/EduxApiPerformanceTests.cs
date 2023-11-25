771635b3693a6f432527c0fc1fb2446de1899f89
Wiktor Muller <biznes.wiktor@gmail.com>
2023-11-25 14:09:34 +0100

    fix(tests): Fix problem with running Shared.Tests project as a runnable tests

--- tests/Modules/Users/Edux.Modules.Users.Tests.Performance/EduxApiPerformanceTests.cs
+++ tests/Modules/Users/Edux.Modules.Users.Tests.Performance/EduxApiPerformanceTests.cs
@@ -14,42 +14,42 @@ namespace Edux.Modules.Users.Tests.Performance
             _outputHelper = outputHelper;
         }
 
-        //[Fact]
-        //public void root_endpoint_from_bootstrapper_handle_at_least_100_requests_per_seconds()
-        //{
-        //    // Arrange
-        //    const string url = "http://localhost:5000/";
+        [Fact]
+        public void root_endpoint_from_bootstrapper_handle_at_least_100_requests_per_seconds()
+        {
+            // Arrange
+            const string url = "http://localhost:5000/";
 
-        //    using var httpClient = new HttpClient();
+            using var httpClient = new HttpClient();
 
-        //    const int expectedRequestsPerSeconds = 100;
-        //    const int durationSeconds = 5;
+            const int expectedRequestsPerSeconds = 100;
+            const int durationSeconds = 5;
 
-        //    var getRootScenario = Scenario.Create("get root scenario", async context =>
-        //    {
-        //        try
-        //        {
-        //            var request = Http.CreateRequest("GET", url);
+            var getRootScenario = Scenario.Create("get root scenario", async context =>
+            {
+                try
+                {
+                    var request = Http.CreateRequest("GET", url);
 
-        //            var response = await Http.Send(httpClient, request);
-        //            return response;
-        //        }
-        //        catch
-        //        {
-        //            return Response.Fail();
-        //        }
-        //    })
-        //    .WithWarmUpDuration(TimeSpan.FromSeconds(5))
-        //    .WithLoadSimulations(Simulation.KeepConstant(copies: expectedRequestsPerSeconds, during: TimeSpan.FromSeconds(durationSeconds)));
+                    var response = await Http.Send(httpClient, request);
+                    return response;
+                }
+                catch
+                {
+                    return Response.Fail();
+                }
+            })
+            .WithWarmUpDuration(TimeSpan.FromSeconds(5))
+            .WithLoadSimulations(Simulation.KeepConstant(copies: expectedRequestsPerSeconds, during: TimeSpan.FromSeconds(durationSeconds)));
 
-        //    // Act
-        //    var stats = NBomberRunner
-        //        .RegisterScenarios(getRootScenario)
-        //        .Run();
+            // Act
+            var stats = NBomberRunner
+                .RegisterScenarios(getRootScenario)
+                .Run();
 
-        //    // Assert
-        //    _outputHelper.WriteLine($"OK: {stats.AllOkCount}, FAILED: {stats.AllFailCount}.");
-        //    stats.AllOkCount.ShouldBeGreaterThanOrEqualTo(durationSeconds * expectedRequestsPerSeconds);
-        //}
+            // Assert
+            _outputHelper.WriteLine($"OK: {stats.AllOkCount}, FAILED: {stats.AllFailCount}.");
+            stats.AllOkCount.ShouldBeGreaterThanOrEqualTo(durationSeconds * expectedRequestsPerSeconds);
+        }
     }
 }
