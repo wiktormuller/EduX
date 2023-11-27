87d9170897b3e284ca8202b81121a29eb1ca49dc
Wiktor Muller <biznes.wiktor@gmail.com>
2023-11-27 20:35:38 +0100

    tests(shared-tests): Extend test containers with Redis and MongoDB

--- src/Shared/Edux.Shared.Tests/EduxWebApplicationFactory.cs
+++ src/Shared/Edux.Shared.Tests/EduxWebApplicationFactory.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Hosting;
 using Testcontainers.MsSql;
 using DotNet.Testcontainers.Builders;
 using Testcontainers.RabbitMq;
+using Testcontainers.MongoDb;
+using Testcontainers.Redis;
 
 namespace Edux.Shared.Tests
 {
@@ -21,16 +23,24 @@ namespace Edux.Shared.Tests
 
         private readonly MsSqlContainer _msSqlContainer;
         private readonly RabbitMqContainer _rabbitMqContainer;
-        private readonly static string ConnectionStringKey = "sqlserver:connectionString";
-        private readonly static string HostNameKey = "rabbitmq:hostnames";
-        private readonly static string PortKey = "rabbitmq:port";
-        private readonly static string UsernameKey = "rabbitmq:username";
-        private readonly static string PasswordKey = "rabbitmq:password";
+        private readonly MongoDbContainer _mongoDbContainer;
+        private readonly RedisContainer _redisContainer;
+
+        private readonly static string SqlServerConnectionStringKey = "sqlserver:connectionString";
+
+        private readonly static string RabbitHostNameKey = "rabbitmq:hostnames";
+        private readonly static string RabbitPortKey = "rabbitmq:port";
+        private readonly static string RabbitUsernameKey = "rabbitmq:username";
+        private readonly static string RabbitPasswordKey = "rabbitmq:password";
+
+        private readonly static string RedisConnectionStringKey = "redis:connectionString";
+
+        private readonly static string MongoConnectionStringKey = "mongo:connectionString";
 
         public EduxWebApplicationFactory()
         {
             _msSqlContainer = new MsSqlBuilder()
-                .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
+                .WithImage("mcr.microsoft.com/mssql/server:2022-preview-ubuntu-22.04")
                 .WithPassword("Password123")
                 .WithCleanUp(true)
                 .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(1433))
@@ -42,20 +52,39 @@ namespace Edux.Shared.Tests
                 .WithPassword("guest")
                 .WithCleanUp(true)
                 .Build();
+
+            _redisContainer = new RedisBuilder()
+                .WithImage("redis:7.2.3")
+                .WithCleanUp(true)
+                .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(6379))
+                .Build();
+
+            _mongoDbContainer = new MongoDbBuilder()
+                .WithImage("mongo-db:windowsservercore-ltsc2022")
+                .WithCleanUp(true)
+                .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(27017))
+                .Build();
         }
 
         protected override void ConfigureWebHost(IWebHostBuilder webHostBuilder) // Before application gets built
         {
             webHostBuilder.UseEnvironment("test");
 
-            // All components that use the SqlConnectionString will use the version from TestContainer
-            webHostBuilder.UseSetting(ConnectionStringKey, _msSqlContainer.GetConnectionString());
+            // SQL Server
+            webHostBuilder.UseSetting(SqlServerConnectionStringKey, _msSqlContainer.GetConnectionString());
 
+            // RabbitMQ
             var uri = new Uri(_rabbitMqContainer.GetConnectionString());
-            webHostBuilder.UseSetting(HostNameKey, uri.Host);
-            webHostBuilder.UseSetting(PortKey, uri.Port.ToString());
-            webHostBuilder.UseSetting(UsernameKey, GetUsernameFromUserInfo(uri.UserInfo));
-            webHostBuilder.UseSetting(PasswordKey, GetPasswordFromUserInfo(uri.UserInfo));
+            webHostBuilder.UseSetting(RabbitHostNameKey, uri.Host);
+            webHostBuilder.UseSetting(RabbitPortKey, uri.Port.ToString());
+            webHostBuilder.UseSetting(RabbitUsernameKey, GetUsernameFromUserInfo(uri.UserInfo));
+            webHostBuilder.UseSetting(RabbitPasswordKey, GetPasswordFromUserInfo(uri.UserInfo));
+
+            // Redis
+            webHostBuilder.UseSetting(RedisConnectionStringKey, _redisContainer.GetConnectionString());
+
+            // MongoDB
+            webHostBuilder.UseSetting(MongoConnectionStringKey, _mongoDbContainer.GetConnectionString());
         }
 
         public async Task InitializeAsync()
